9feda19456d07a89f23b52aa86b035df25057415
pp-amaskaev <amaskaev@pulsepoint.com>
2024-07-24 21:14:21 +0100

    adding utils for dataset processing

--- src/hse_deep_learning/utils/dataset.py
+++ src/hse_deep_learning/utils/dataset.py
@@ -6,11 +6,47 @@ import cv2
 import numpy as np
 from pydantic import BaseModel
 
+from hse_deep_learning.utils.shapes import Rect
+
+
+class GroundTruth:
+    def __init__(self, ground_truth_file: str):
+        if not os.path.exists(ground_truth_file):
+            raise IOError
+
+        self.raw_data = np.loadtxt(ground_truth_file, delimiter=',')
+        self.frames_lookup: dict[int, dict[int, Rect]] = dict()
+        self.tracks_lookup: dict[int, list[tuple[int, Rect]]] = dict()
+
+        for entry in self.raw_data:
+            frame_id, track_id, bbox, confidence = int(entry[0]), int(entry[1]), entry[2:6], entry[6]
+
+            should_consider = confidence == 1
+
+            if not should_consider:
+                continue
+
+            if frame_id not in self.frames_lookup:
+                self.frames_lookup[frame_id] = dict()
+
+            if track_id not in self.tracks_lookup:
+                self.tracks_lookup[track_id] = list()
+
+            bbox_rect = Rect(left=bbox[0], top=bbox[1], width=bbox[2], height=bbox[3])
+            self.frames_lookup[frame_id][track_id] = bbox_rect
+            self.tracks_lookup[track_id].append((frame_id, bbox_rect))
+
+    def __getitem__(self, frame_id: int) -> dict[int, Rect]:
+        return self.frames_lookup.get(frame_id, dict())
+
+    def get_track(self, track_id: int) -> list[tuple[int, Rect]]:
+        return self.tracks_lookup[track_id]
+
 
 class DatasetProcessor(BaseModel):
     name: str
     images_files: List[str]
-    ground_truth: Optional[MotGroundTruth]
+    ground_truth: Optional[GroundTruth]
     detections: np.ndarray
     image_size: Optional[np.ndarray]
     update_rate: Optional[float]
@@ -21,7 +57,7 @@ def load(sequence_directory: str) -> DatasetProcessor:
     images_files = sorted([os.path.join(images_directory, file) for file in os.listdir(images_directory)])
     ground_truth_file = Path(sequence_directory) / "gt" / "gt.txt"
 
-    ground_truth = MotGroundTruth.load(ground_truth_file)
+    ground_truth = GroundTruth(str(ground_truth_file))
 
     detection_file = os.path.join(sequence_directory, "det", "det.txt")
     detections = np.loadtxt(detection_file, delimiter=",")
